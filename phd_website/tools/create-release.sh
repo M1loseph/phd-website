0080d480915f9318e38eab76c5b95a6d16c995db
M1loseph <milosz.dyka@gmail.com>
2024-01-16 08:09:33 +0100

    some fixes in build runner

--- phd_website/tools/create-release.sh
+++ phd_website/tools/create-release.sh
@@ -3,4 +3,8 @@
 # This script should be run in the root of flutter project.
 set -e
 
+flutter pub get
+# It looks like cleaning is mandatory, otherwise the generated file will not change
+dart run build_runner clean
+dart run build_runner build --delete-conflicting-outputs
 flutter build web
