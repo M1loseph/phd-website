bb68fb16c6b11dbda7fcd89167c30fca7fac5b2f
M1loseph <milosz.dyka@gmail.com>
2024-10-02 16:31:50 +0200

    new conference

--- phd_website/lib/model/conference_do.dart
+++ phd_website/lib/model/conference_do.dart
@@ -2,14 +2,16 @@ class ConferenceDO {
   final String conferenceName;
   final Uri website;
   final String talkTitle;
-  final DateTime date;
+  final DateTime begin;
+  final DateTime end;
   final String location;
 
   ConferenceDO({
     required this.conferenceName,
     required String website,
     required this.talkTitle,
-    required this.date,
+    required this.begin,
+    required this.end,
     required this.location,
   }) : website = Uri.parse(website);
 }
--- phd_website/lib/services/analytics_service.dart
+++ phd_website/lib/services/analytics_service.dart
@@ -50,8 +50,8 @@ class AnalyticsService {
       } catch (e) {
         if (kDebugMode) {
           logger.debug('Error occurred when establishing session $e');
-          await Future.delayed(const Duration(seconds: 1));
         }
+        await Future.delayed(const Duration(seconds: 5));
       }
     }
 
