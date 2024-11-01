8960a4559af15fc6b6ea0ff8f91bf58c83dfe3b1
M1loseph <milosz.dyka@gmail.com>
2024-11-01 20:15:18 +0100

    don't use flutter inside build runner

--- phd_website/lib/build_properties/build_properties_generator.dart
+++ phd_website/lib/build_properties/build_properties_generator.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:build/build.dart';
-import 'package:phd_website/logger/logger.dart';
 import 'package:source_gen/source_gen.dart';
 
 abstract class VersionSourceRunner {
@@ -12,8 +11,6 @@ abstract class VersionSourceRunner {
 class GitVersionSourceRunner implements VersionSourceRunner {
   static const appNameSeparator = '/';
 
-  final logger = Logger(GitVersionSourceRunner);
-
   @override
   FutureOr<String> getGitVersion() async {
     final processResult = await Process.run('git', [
@@ -22,16 +19,14 @@ class GitVersionSourceRunner implements VersionSourceRunner {
       '--match="phdwebsite/**"',
     ]);
     if (processResult.exitCode != 0) {
-      logger.error(
+      throw Exception(
           'Error occurred when running git. Exit code: ${processResult.exitCode}. Stderr: ${processResult.stderr}');
-      throw Exception('Error executing git command');
     }
     final version = (processResult.stdout as String).trim();
     final partsIndex = version.indexOf(appNameSeparator);
     if (version.isEmpty ||
         !version.startsWith('phdwebsite') ||
         partsIndex == -1) {
-      logger.error('Unexpected version format: $version');
       throw Exception('Unexpected version format: $version');
     }
     return version.substring(partsIndex + 1);
