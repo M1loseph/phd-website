98e01bd15ce5dcda4170fed06d3d3e89e245262f
Miłosz Dyka <milosz.dyka@gmail.com>
2025-09-22 21:15:20 +0200

    add timeouts to the http client

--- phd_website/lib/services/analytics_service.dart
+++ phd_website/lib/services/analytics_service.dart
@@ -3,12 +3,11 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
+import 'package:http_status/http_status.dart';
 import 'package:phd_website/build_properties/build_properties.dart';
 import 'package:phd_website/clock/clock.dart';
 import 'package:phd_website/logger/logger.dart';
 import 'package:phd_website/services/analytics_events.dart';
-
-const httpCreated = 201;
 
 // TODO: maybe try not to do heavy lifting in constructor - just for the sake of not doing it :)
 class AnalyticsService {
@@ -39,17 +38,26 @@ class AnalyticsService {
     while (sessionId == null) {
       try {
         final event = AppOpenedEvent(
-            clock.now(), environment, buildProperties.appVersion);
+          clock.now(),
+          environment,
+          buildProperties.appVersion,
+        );
         final response = await httpClient.post(
           analyticsUrl.replace(path: 'api/v1/analytics/appOpened'),
           headers: {'Content-Type': 'application/json'},
           body: json.encode(event.toJson()),
         );
-        if (response.statusCode != httpCreated) {
+        final responseStatus = HttpStatus.fromCode(response.statusCode);
+        if (responseStatus != HttpStatus.created) {
+          if (kDebugMode) {
+            logger.debug('Got response $response');
+          }
+          await Future.delayed(const Duration(seconds: 5));
           continue;
         }
-        final appOpenedEventResponse =
-            AppOpenedEventResponse.fromJson(json.decode(response.body));
+        final appOpenedEventResponse = AppOpenedEventResponse.fromJson(
+          json.decode(response.body),
+        );
         sessionId = appOpenedEventResponse.sessionId;
       } catch (e) {
         if (kDebugMode) {
@@ -63,9 +71,7 @@ class AnalyticsService {
       final event = PageOpenedEvent(sessionId, openTime, pageData);
       await httpClient.post(
         analyticsUrl.replace(path: 'api/v1/analytics/pageOpened'),
-        headers: Map.from(
-          {'Content-Type': 'application/json'},
-        ),
+        headers: Map.from({'Content-Type': 'application/json'}),
         body: json.encode(event.toJson()),
       );
     }
