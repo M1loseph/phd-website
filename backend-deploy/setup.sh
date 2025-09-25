46916a5e3153a7972eb7e405c848531d75fcec77
Miłosz Dyka <milosz.dyka@gmail.com>
2025-09-25 21:49:05 +0200

    remove deployment configuration from this repository

--- backend-deploy/setup.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-sudo snap install certbot --classic
-
-sudo certbot obtain -d phdwebsite.duckdns.org
-
-sudo ln -s /etc/letsencrypt/live/phdwebsite.duckdns.org/privkey.pem privkey.pem
-sudo ln -s /etc/letsencrypt/live/phdwebsite.duckdns.org/fullchain.pem fullchain.pem
