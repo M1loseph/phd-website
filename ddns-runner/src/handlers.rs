95aa1ae0de96ea1ad7cabaae3d95b2dce94f0597
Miłosz Dyka <milosz.dyka@gmail.com>
2025-10-20 17:39:22 +0200

    apply copilot suggestions

--- ddns-runner/src/handlers.rs
+++ ddns-runner/src/handlers.rs
@@ -1,22 +1,35 @@
 use std::sync::Arc;
 
 use axum::extract::State;
-use axum::http::{header, HeaderMap};
+use axum::http::{header, HeaderMap, HeaderValue};
+use log::error;
 use prometheus::TextEncoder;
+use reqwest::StatusCode;
 
 pub struct AppState {
     pub registry: prometheus::Registry,
 }
 
-pub async fn prometheus_handler(State(state): State<Arc<AppState>>) -> (HeaderMap, String) {
+pub async fn prometheus_handler(
+    State(state): State<Arc<AppState>>,
+) -> Result<(HeaderMap, String), (StatusCode, &'static str)> {
     let encoder = TextEncoder::new();
     let metric_families = state.registry.gather();
-    let response = encoder.encode_to_string(&metric_families).unwrap();
+    let response = match encoder.encode_to_string(&metric_families) {
+        Ok(v) => v,
+        Err(e) => {
+            error!("Error occurred while encoding metrics to string: {}", e);
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Error occurred while encoding metrics",
+            ));
+        }
+    };
 
     let mut headers = HeaderMap::new();
     headers.insert(
         header::CONTENT_TYPE,
-        "text/plain; version=0.0.4".parse().unwrap(),
+        HeaderValue::from_static("text/plain; version=0.0.4"),
     );
-    (headers, response)
+    Ok((headers, response))
 }
