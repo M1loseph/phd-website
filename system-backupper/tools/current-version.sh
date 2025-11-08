e177584bd95c92ad93590340fa7bde476a20933f
Miłosz Dyka <milosz.dyka@gmail.com>
2025-11-08 13:33:11 +0100

    deploy of system backupper

---
+++ system-backupper/tools/current-version.sh
@@ -0,0 +1,13 @@
+set -e 
+
+GIT_VERSION=$(git describe --tags --abbrev=0)
+
+if [[ "$GIT_VERSION" != system-backupper/* ]]
+then
+    echo "Incorrect version $GIT_VERSION"
+    exit 1
+fi
+
+PURE_VERSION=$(echo "$GIT_VERSION" | cut -d '/' -f 2)
+
+echo "$PURE_VERSION"
