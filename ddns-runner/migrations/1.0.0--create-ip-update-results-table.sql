76434dcb2269f8a70e345252faca10d76b14b982
Miłosz Dyka <milosz.dyka@gmail.com>
2026-07-07 16:18:50 +0200

    remove ddns runner

--- ddns-runner/migrations/1.0.0--create-ip-update-results-table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "IpUpdateResults" (
-  "id" BIGSERIAL PRIMARY KEY,
-  "server_action" VARCHAR(30) NOT NULL,
-  "ipv4" INET NOT NULL,
-  "ipv6" INET,
-  "inserted_at" TIMESTAMP NOT NULL
-)
